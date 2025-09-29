e262c6b9084eb33904f849b193f894a5eb9f34a4
Calvin-0123 <calvin.li1@pcc.edu>
2025-09-29 09:22:49 +0000

    update homework

--- homework-payroll.cpp
+++ homework-payroll.cpp
@@ -45,70 +45,37 @@ using namespace std;
 int main(int argc, char* argv[]) {
 // Setting base
   int ID = 0;
-  float hours = 0.0;
-  float salaryperhour = 0.0;
-  int taxrate = 0;
-  float total = 0.0;
-  float tax = 0.0;
-  float net = 0.0;
+  float Hours = 0;
+  float Salaryperhour = 0;
+  int Taxrate = 0;
+  float Total = 0;
+  float Tax = 0;
+  float Net = 0;
 
 // user input
 
   cout << "Enter your ID number: ";
   cin >> ID;
   cout << "Enter your working hours: ";
-  cin >> hours;
+  cin >> Hours;
   cout << "Enter your salary per hour: ";
-  cin >> salaryperhour;
+  cin >> Salaryperhour;
   cout << "Enter federal withholding rate: ";
-  cin >> taxrate;
+  cin >> Taxrate;
 
 // calculating
 
-  total = hours * salaryperhour;
-  tax = (total * taxrate) / 100;
-  net = total - tax;
+  Total = Hours * Salaryperhour;
+  Tax = (Total * Taxrate) / 100;
+  Net = Total - Tax;
 
 //output
 
   cout << "Welcome to your payroll calculator!" << endl;
-  cout << "Your total gross pay is: " << total << endl;
-  cout << "Your tax amount is: " << tax << endl;
-  cout << "Your Total net pay is: " << net << endl;
+  cout << "Your total gross pay is: " << Total << endl;
+  cout << "Your tax amount is: " << Tax << endl;
+  cout << "Your Total net pay is: " << Net << endl;
   cout << "Thanks for using this program!" << endl;
 
   return 0;
 }
-
-// Function implementations (if any)
-
-
-// ------------- DESIGN -------------
-/* 
-Program Name:
-
-Program Description:
-
-Design:
-A. INPUT
-Define the input variables including name data type. 
-
-B. OUTPUT
-Define the output variables including data types. 
-
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
-
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
-
-SAMPLE RUNS
-Copy from assignment document.
-
-*/

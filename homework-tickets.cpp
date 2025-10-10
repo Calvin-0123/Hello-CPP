c7df0b8aa9d5deea6b9471905ae6d1aff2af7e76
Calvin-0123 <calvin.li1@pcc.edu>
2025-10-10 05:40:35 +0000

    final version for tickets

--- homework-tickets.cpp
+++ homework-tickets.cpp
@@ -53,12 +53,13 @@ int main() {
   int bikes = 0;
   double total;
   double hun;
+  int num_people;
 
   cout << "Welcome to your tickets calculator! Using Y/N to answer questions!" << endl;
   cout << endl;
   cout << "Fare Description" << endl;
   cout << "--------------------------------------------    ------" << endl;
-  cout << "Vehicle Under 14' (less than 168\") & Driver.    $57.90" << endl;
+  cout << "Vehicle Under 14' (less than 168\") & Driver    $57.90" << endl;
   cout << "Adult (age 19 - 64)                             $14.95" << endl;
   cout << "Senior (age 65 & over) / Disability             $7.40" << endl;
   cout << "Youth (age 6 - 18)                              $5.55" << endl;
@@ -69,57 +70,97 @@ int main() {
   cout << "Did you ride a vehicle?" << endl;
   if (!(cin >> an)) { 
     cout << "Wrong input, please try again later!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
     return 0;
   }
     an = static_cast<char>(tolower(an));
  
   if (an != 'y' && an != 'n'){
     cout << "Wrong input, please try again later!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
     return 0;
   }
-
-
-
+  
   if (an == 'y') {
     v_ad = 1;
     bikes = 0;
+
     cout << "How many adults you have?" << endl;
-    cin >> ad;
+    if (!(cin >> ad) || ad < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+    }
     if (ad > 0) {
       ad -=1;
     }
     cout << "How many seniors you have?" << endl;
-    cin >> sen;
-    cout << "How many youths you have?" << endl;
-    cin >> you;
-  } 
+    if (!(cin >> sen) || sen < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+    }
 
-  else if (an == 'n') {
-    v_ad = 0;
-    cout << "How many adults you have?" << endl;
-    cin >> ad;
-    cout << "How many seniors you have?" << endl;
-    cin >> sen;
     cout << "How many youths you have?" << endl;
-    cin >> you;
-    cout << "How many bikes you have?" << endl;
-    cin >> bikes;
+    if (!(cin >> you) || you < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+    }
   }
+    else if (an == 'n') {
+    v_ad = 0;
+    
+    cout << "How many adults you have?" << endl;
+    if (!(cin >> ad) || ad < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+    }
+    cout << "How many seniors you have?" << endl;
+    if (!(cin >> sen) || sen < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+  }
+    cout << "How many youths you have?" << endl;
+    if (!(cin >> you) || you < 0) {
+    cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+  }
+    cout << "How many bikes you have?" << endl;
+    if (!(cin >> bikes) || bikes < 0) {
+      cout << "Wrong input, please try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+  }
+}
+  num_people = ad + sen + you;
+
+  if (num_people > 20) {
+    cout << "Uh oh!! Too many people in your group. Split into 2 groups and try again!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
+    return 0;
+  } 
 
   total =  v_ad_p * v_ad + ad_p * ad + sen_p * sen + you_p * you + bikes_p * bikes;
   if (total < 100){
     hun = 100 - total;
     cout << endl;
     cout << fixed << showpoint << setprecision(2);
-    cout << "Your total charges will be " << total << endl;
+    cout << "Your total charges will be $" << total << endl;
     cout << endl;
-    cout << "If you spend " << hun << " more, you will get a free adult ticket!" << endl;
+    cout << "If you spend $" << hun << " more, you will get a free adult ticket!" << endl;
     cout << endl;
-    cout << "Thank you for using Washington State Ferries Fare Calculator!";
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
   }
   else if (total >= 100){
-    cout << "You total charges will be " << total << endl;
-    cout << "Thank you for using Washington State Ferries Fare Calculator!";
+    cout << fixed << showpoint << setprecision(2);
+    cout << "Your total charges will be $" << total << endl;
+    cout << "                      ****                 ******************" << endl;
+    cout << "Your purchase is over $100, you will get a free adult ticket!" << endl;
+    cout << "Thank you for using Washington State Ferries Fare Calculator!" << endl;
   }
 
   return 0;

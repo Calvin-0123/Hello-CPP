c9251015d34f47593c7a992468ace9c2ce595149
Calvin-0123 <calvin.li1@pcc.edu>
2025-10-14 20:58:57 +0000

    update

--- homework-find.cpp
+++ homework-find.cpp
@@ -22,7 +22,7 @@
 // A. INPUT ✅: Two phrases
 // B. OUTPUT ✅:Check same or one of them in another one.
 // C. CALCULATIONS ✅:None
-// D. LOGIC and ALGORITHMS ✅:Using if statement to check same or one of them in another one.
+// D. LOGIC and ALGORITHMS ✅:Using if and find statement to check same or one of them in another one.
 //    (Optional) flow chart link or file name: 
 
 
@@ -54,24 +54,26 @@ int main() {
   cout << "Welcome to the finder." << endl;
   cout << "Please enter your first phrase: ";
   getline(cin, phrase1);
-  cout << "Your entered: " << phrase1 << endl;
+  cout << "You entered: " << phrase1 << endl;
+  cout << "Please enter your second phrase: ";
   getline(cin, phrase2);
+  cout << "You entered: " << phrase2 << endl;
 
   if (phrase1 == phrase2){
     cout << "They are same.";
   }
   else {
-    size_t pos12 = phrase1.find(phrase2);
-    size_t pos21 = phrase2.find(phrase1);
+    size_t pos12 = phrase2.find(phrase1);
+    size_t pos21 = phrase1.find(phrase2);
 
-    if (pos12 != str::npos){
+    if (pos12 != string::npos){
 
         cout << phrase1 << " is found in " << phrase2 << endl;
-        cout << phrase1.substr(pos12) << endl;
+        cout << phrase2.substr(pos12) << endl;
     }
-    else if (pos21 != str::npos){
+    else if (pos21 != string::npos){
         cout << phrase2 << " is found in " << phrase1 << endl;
-        cout << phrase2.substr(pos21) << endl;
+        cout << phrase1.substr(pos21) << endl;
     }
     else {
         cout << "No match" << endl;
@@ -88,30 +90,46 @@ int main() {
 
 // ------------- DESIGN -------------
 /* 
-Program Name:
+Program Name: Finder
 
-Program Description:
+Program Description: It can find a phrase in another phrase
 
 Design:
-A. INPUT
+A. INPUT 
 Define the input variables including name data type. 
+    Two phrases
 
 B. OUTPUT
 Define the output variables including data types. 
+    Same, phrase1 is found in phrase2, phrase2 is found in phrase1, not match
 
 C. CALCULATIONS
 Describe calculations used by algorithms in step D.  
 List all formulas. 
 If there are no calculations needed, state there are no calculations.
+    None
 
 D. LOGIC and ALGORITHMS
 Design the logic of your program using pseudocode or flowcharts. 
 Use conditionals, loops, functions or array constructs.
 List the steps in transforming inputs into outputs. 
 https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+    Using if and find statement to check same or one of them in another one.
 
 
 SAMPLE RUNS
 Copy from assignment document.
+
+Welcome to the finder.
+Please enter your first phrase: I do not know.
+You entered: I do not know.
+Please enter your second phrase: no
+You entered: no
+no is found in I do not know.
+not know.
+
+Thanks for using the finder!
+ 
+Normal program termination. Exit status: 0
 
 */

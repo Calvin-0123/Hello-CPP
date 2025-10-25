3f56d41574a43aabf533556e5ba87c32100f0473
Calvin-0123 <calvin.li1@pcc.edu>
2025-10-25 07:33:12 +0000

    final

--- midterm.cpp
+++ midterm.cpp
@@ -19,10 +19,10 @@
 
 
 // ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
-// B. OUTPUT ✅:
-// C. CALCULATIONS ✅:
-// D. LOGIC and ALGORITHMS ✅:
+// A. INPUT ✅: names and numbers.
+// B. OUTPUT ✅: their order and the average
+// C. CALCULATIONS ✅:comparison and the average
+// D. LOGIC and ALGORITHMS ✅:To compare their orders and list them. Tells the average number.
 //    (Optional) flow chart link or file name: 
 
 
@@ -58,36 +58,60 @@ int main() {
     int e; // third
     string nameee;
 // If different
-    int f; // second
-    int g; // first
+    string f; // second
+    string g; // first
 
     cout << "Welcome!" << endl;
 // Cin names and numbers.
     cout << "Enter player 1 name: " << endl;
-        getline(cin, name1);
+    getline(cin, name1);
     cout << "How many rocks did " << name1 << " collect?" << endl;
-        cin >> rock1;
-    if (rock1 <0 ){
+
+    if (!(cin >> rock1)) {
+        cout << "Invalid amount. 0 will be entered." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         rock1 = 0;
-        cout << "Not a positive number, set for 0." << endl;
+    } else {
+        if (rock1 < 0) {
+            rock1 = 0;
+            cout << "Not a positive number, set for 0." << endl;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 成功读取后清掉换行
     }
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
     cout << "Enter player 2 name: " << endl;
-        getline(cin, name2);
+    getline(cin, name2);
     cout << "How many rocks did " << name2 << " collect?" << endl;
-        cin >> rock2;
-    if (rock2 <0 ){
+
+    if (!(cin >> rock2)) {
+        cout << "Invalid amount. 0 will be entered." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         rock2 = 0;
-        cout << "Not a positive number, set for 0." << endl;
+    } else {
+        if (rock2 < 0) {
+            rock2 = 0;
+            cout << "Not a positive number, set for 0." << endl;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
     cout << "Enter player 3 name: " << endl;
-        getline(cin, name3);
+    getline(cin, name3);
     cout << "How many rocks did " << name3 << " collect?" << endl;
-        cin >> rock3;
-    if (rock3 <0 ){
+
+    if (!(cin >> rock3)) {
+        cout << "Invalid amount. 0 will be entered." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         rock3 = 0;
-        cout << "Not a positive number, set for 0." << endl;
+    } else {
+        if (rock3 < 0) {
+            rock3 = 0;
+            cout << "Not a positive number, set for 0." << endl;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     cout << fixed << setprecision(2);
@@ -105,12 +129,12 @@ int main() {
     if (rock1 == rock2){
         if (rock2 > rock3){
             cout << name1 << " and " << name2 << " are same!" << endl;
-            f = rock3;
+            f = name3;
             cout << f << " will be the second!" << endl;
         }
-        else {g = rock3;
+        else {g = name3;
              cout << g << " got the first!" << endl;
-             cout << rock1 << " and " << rock2 << " are same!" << endl;
+             cout << name1 << " and " << name2 << " are same!" << endl;
         }
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
@@ -119,12 +143,12 @@ int main() {
     else if (rock2 == rock3){
         if (rock2 > rock1){
             cout << name2 << " and " << name3 << " are same!" << endl;
-            f = rock1;
+            f = name1;
             cout << f << " will be the second!" << endl;
         }
-        else {g = rock1;
+        else {g = name1;
              cout << g << " got the first!" << endl;
-             cout << rock2 << " and " << rock3 << " are same!" << endl;
+             cout << name2 << " and " << name3 << " are same!" << endl;
         }
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
@@ -133,12 +157,12 @@ int main() {
     else if (rock1 == rock3){
         if (rock1 > rock2){
             cout << name1 << " and " << name3 << " are same!" << endl;
-            f = rock2;
+            f = name2;
             cout << f << " will be the second!" << endl;
         }
-        else {g = rock2;
+        else {g = name2;
              cout << g << " got the first!" << endl;
-             cout << rock1 << " and " << rock3 << " are same!" << endl;
+             cout << name1 << " and " << name3 << " are same!" << endl;
         }
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
@@ -166,7 +190,7 @@ int main() {
             d = rock3;  namee = name3;
             e = rock2;  nameee = name2;
         }
-        cout << c << " got first! " << d << " got second! " << e << " got third!" << endl;
+        cout << name << " got first! " << namee << " got second! " << nameee << " got third!" << endl;
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
         return 0;
@@ -179,7 +203,7 @@ int main() {
             d = rock3;  namee = name3;
             e = rock1;  nameee = name1;
         }
-        cout << c << " got first! " << d << " got second! " << e << " got third!" << endl;
+        cout << name << " got first! " << namee << " got second! " << nameee << " got third!" << endl;
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
         return 0;
@@ -192,26 +216,11 @@ int main() {
             d = rock2;  namee = name2;
             e = rock1;  nameee = name1;
         }
-        cout << c << " got first! " << d << " got second! " << e << " got third!" << endl;
+        cout << name << " got first! " << namee << " got second! " << nameee << " got third!" << endl;
         cout << fixed << setprecision(2);
         cout << "The average is " << ave << endl;
         return 0;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
 
@@ -227,23 +236,41 @@ Program Description:
 Design:
 A. INPUT
 Define the input variables including name data type. 
-
+ names and numbers.
 B. OUTPUT
 Define the output variables including data types. 
-
+ their order and the average
 C. CALCULATIONS
 Describe calculations used by algorithms in step D.  
 List all formulas. 
 If there are no calculations needed, state there are no calculations.
-
+ comparison and the average
 D. LOGIC and ALGORITHMS
 Design the logic of your program using pseudocode or flowcharts. 
 Use conditionals, loops, functions or array constructs.
 List the steps in transforming inputs into outputs. 
 https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
+To compare their orders and list them. Tells the average number.
 
 SAMPLE RUNS
 Copy from assignment document.
+
+Welcome!
+Enter player 1 name: 
+li
+How many rocks did li collect?
+2
+Enter player 2 name: 
+wang
+How many rocks did wang collect?
+3
+Enter player 3 name: 
+ni
+How many rocks did ni collect?
+4
+ni got first! wang got second! li got third!
+The average is 3.00
+ 
+Normal program termination. Exit status: 0
 
 */

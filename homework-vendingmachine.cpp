13077c242f06b3fe7edba5087017bea7ec775e30
Calvin-0123 <calvin.li1@pcc.edu>
2025-11-09 12:36:46 +0000

    update

--- homework-vendingmachine.cpp
+++ homework-vendingmachine.cpp
@@ -53,66 +53,15 @@ int main() {
   char choice;            // Letter answered
   int quantity = 0;       // Number of items
   int total = 0;          // Total price
-  bool running = ture     // For exit the loop
+  bool running = true;     // For exit the loop
 
 // Start
-  cout << "Welcome to the vending machine!" << endl;
+  while (running) {
+    cout << "Welcome to the vending machine!" << endl;
 
 // Insert coins
-  do {
-
-    cout << "Enter coins - 5, 10, 25 only, 0 to stop: ";
-    cin >> coins;
-    while ((!cin) || (coins!= 0 && coins != 5 && coins != 10 && coins != 25)){ //check if valid
-      cin.clear();                                                             //clear the wrong status
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');                     //clear what just cin until the enter
-      cout << "Invalid coin!";
-      cout << "Please try again!";
-      cout << "Enter coins - 5, 10, 25 only, 0 to stop: ";
-      cin >> coins;
-    }                                                                          // exit the loop
-    balance += coins;
-    cout << "Your balance for now is: " << fixed << setprecision(2) << (balance / 100.0) << endl;
-  } while ( coins != 0 );
-  cout << "\nYour balance is $" << fixed << setprecision(2) << (balance / 100.0) << "\n\n";
-
-// Choose items
-  cout << "Please pick an option ($0.25 each): C/c: Coffee, T/t: Tea, Q/q: Quit~\n";
-  cin >> choice;
-  choice = tolower(choice);
-
-  while (choice != 'c' && choice != 't' && choice != 'q') {
-    cout << "Invalid option!" << endl;
-    cout << "Please try again!" << endl;
-    cout << "Please pick an option ($0.25 each): C/c: Coffee, T/t: Tea, Q/q: Quit\n";
-    cin >> choice;
-    choice = tolower(choice);
-  }                                                                         
-
-  switch (choice) {
-    case 'c':
-      cout << "Your choice is Coffee\n";
-      break;
-    case 't':
-      cout << "Your choice is Tea\n";
-      break;
-    case 'q':
-      cout << "Your choice is quit\n";
-      cout << "Your balance is $" << fixed << setprecision(2) << (balance / 100.0) << endl;
-      cout << "Thank you for using my vending machine!\n";
-      running = false; // ✅ 让循环结束
-      break;
-      default:
-      cout << "Invalid choice!\n";
-      break;
-  }                     //chioce == c
-  cout << "How many do you want?";
-  cin >> quantity;
-  total = (choice * quantity) / 100;
-
-  while (total > balance) {
     do {
-      cout << "You do not have enough balance, please enter coins." << endl;
+
       cout << "Enter coins - 5, 10, 25 only, 0 to stop: ";
       cin >> coins;
       while ((!cin) || (coins!= 0 && coins != 5 && coins != 10 && coins != 25)){ //check if valid
@@ -124,15 +73,72 @@ int main() {
         cin >> coins;
       }                                                                          // exit the loop
       balance += coins;
-      cout << "Your balance for now is: " << fixed << setprecision(2) << (balance / 100.0) << endl;
+      if (coins != 0) {
+        cout << "Your balance for now is: $" << fixed << setprecision(2) << (balance / 100.0) << endl;
+      }  
     } while ( coins != 0 );
     cout << "\nYour balance is $" << fixed << setprecision(2) << (balance / 100.0) << "\n\n";
-  }
-  cout << "Your total will be: " << total << endl;
-  balance -= total;
-  cout << "Your balance is: " << balance << endl;
-  cout << "Thanks for using the vending machine.";
 
+// Choose items
+    cout << "Please pick an option ($0.25 each): C/c: Coffee, T/t: Tea, Q/q: Quit~\n";
+    cin >> choice;
+    choice = tolower(choice);
+
+    while (choice != 'c' && choice != 't' && choice != 'q') {
+      cout << "Invalid option!" << endl;
+      cout << "Please try again!" << endl;
+      cout << "Please pick an option ($0.25 each): C/c: Coffee, T/t: Tea, Q/q: Quit\n";
+      cin >> choice;
+      choice = tolower(choice);
+    }                                                                         
+
+    switch (choice) {
+      case 'c':
+        cout << "Your choice is Coffee\n";
+        break;
+      case 't':
+        cout << "Your choice is Tea\n";
+        break;
+      case 'q':
+        cout << "Your choice is quit\n";
+        cout << "Your balance is $" << fixed << setprecision(2) << (balance / 100.0) << endl;
+        cout << "Thank you for using my vending machine!\n";
+        running = false; // Stop the loop
+        break;
+        default:
+        cout << "Invalid choice!\n";
+        break;
+    }
+    cout << "How many do you want?";
+    cin >> quantity;
+    if (choice == 'c') {
+      total = c * quantity;
+    } else {
+      total = t * quantity;
+    }
+    while (total > balance) {
+      do {
+        cout << "You do not have enough balance, please enter coins." << endl;
+        cout << "Enter coins - 5, 10, 25 only, 0 to stop: ";
+        cin >> coins;
+        while ((!cin) || (coins!= 0 && coins != 5 && coins != 10 && coins != 25)){ //check if valid
+          cin.clear();                                                             //clear the wrong status
+          cin.ignore(numeric_limits<streamsize>::max(), '\n');                     //clear what just cin until the enter
+          cout << "Invalid coin!";
+          cout << "Please try again!";
+          cout << "Enter coins - 5, 10, 25 only, 0 to stop: ";
+          cin >> coins;
+        }                                                                          // exit the loop
+        balance += coins;
+        cout << "Your balance for now is: " << fixed << setprecision(2) << (balance / 100.0) << endl;
+      } while ( coins != 0 );
+      cout << "\nYour balance is $" << fixed << setprecision(2) << (balance / 100.0) << "\n\n";
+    }
+    cout << "Your total will be: $" << fixed << setprecision(2) << (total / 100.0) << endl;
+    balance -= total;
+    cout << "Your balance is: " << balance << endl;
+    cout << "Thanks for using the vending machine.";
+  }
 
 
 
